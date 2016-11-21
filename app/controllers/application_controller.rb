8af3fa9b4ce12fb21f343045394009855da8dc68
Benjamin Thomas <benjamin.thomas@ynov.com>
2016-11-21 14:33:44 +0100

    ben

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   I18n.locale = :en
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sin_up,keys: [:first_name,:last_name,:email,:password,:password_confirmation])
+  end
 
 end
